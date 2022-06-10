d1a89afbc962b6bd81ba998750dfcbd2fc0faf6d
MANOJ-51 <m4anoj51@gmail.com>
2022-06-10 19:05:56 +0530

    created contact class

---
+++ com/addressbook/Contact.java
@@ -0,0 +1,13 @@
+package com.addressbook;
+
+public class Contact {
+    private String fullName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private int zip;
+    private long mobileNumber;
+    private String email;
+
+}
