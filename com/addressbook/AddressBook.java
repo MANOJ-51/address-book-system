252839343480edebbec6a424149387a89b81fdae
MANOJ-51 <m4anoj51@gmail.com>
2022-06-10 18:55:02 +0530

    displaying welcome message in master

---
+++ com/addressbook/AddressBook.java
@@ -0,0 +1,8 @@
+package com.addressbook;
+
+public class AddressBook {
+    //DISPLAYING WELCOME MESSAGE
+    public static void main(String[] args) {
+        System.out.println("WELOCOME TO ADRESS BOOK SYSTEM ");
+    }
+}
