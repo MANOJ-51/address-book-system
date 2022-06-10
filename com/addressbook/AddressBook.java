d8d40cc5a223ccd9946cd8a7228161b4bac65681
MANOJ-51 <m4anoj51@gmail.com>
2022-06-10 20:42:26 +0530

    adding contact

--- com/addressbook/AddressBook.java
+++ com/addressbook/AddressBook.java
@@ -1,8 +1,54 @@
 package com.addressbook;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBook {
     //DISPLAYING WELCOME MESSAGE
     public static void main(String[] args) {
         System.out.println("WELOCOME TO ADRESS BOOK SYSTEM ");
+        Contact contact=addContact();
+        System.out.println(contact);
     }
-}
+
+    //taking scanner class for input from the user
+    private static Scanner scanner = new Scanner(System.in);
+
+    //initialisigin array list
+    static ArrayList<Contact> contacts = new ArrayList<Contact>();
+
+    private static Contact addContact() {
+        System.out.println("Enter the First Name :- ");
+        String firstName = scanner.next();
+
+        System.out.println("Enter the Last Name :- ");
+        String lastName = scanner.next();
+
+        System.out.println("Enter the Address :- ");
+        String address = scanner.next();
+
+        System.out.println("Enter the City :- ");
+        String city = scanner.next();
+
+        System.out.println("Enter the State :- ");
+        String state = scanner.next();
+
+        System.out.println("Enter the Zip Code :- ");
+        int zip= scanner.nextInt();
+
+        System.out.println("Enter the Phone Number :- ");
+        long phoneNumber = scanner.nextLong();
+
+        System.out.println("Enter the Email :- ");
+        String email = scanner.next();
+
+        Contact contact=new Contact(firstName,lastName,address,city,state,zip,phoneNumber,email);
+        contacts.add(contact);
+
+        System.out.println("Contact Added Successfully");
+        return contact;
+
+    }
+
+
+    }
--- com/addressbook/Contact.java
+++ com/addressbook/Contact.java
@@ -10,4 +10,92 @@ public class Contact {
     private long mobileNumber;
     private String email;
 
+    public Contact(String fullName, String lastName, String address, String city, String state, int zip, long mobileNumber, String email) {
+        this.fullName = fullName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.mobileNumber = mobileNumber;
+        this.email = email;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public long getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(long mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "fullName='" + fullName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip=" + zip +
+                ", mobileNumber=" + mobileNumber +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
